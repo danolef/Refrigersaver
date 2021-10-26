13dea4ed5933bb0ab77c4edf35bed7a235db9e33
Garrett <garres4@vt.edu>
2021-10-25 19:19:11 -0600

    search works successfully and appends the photo to the dom

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-const baseUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast'
+// DOM Selectors
+const baseUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
+const inputIngredient = document.querySelector('#ingredient-search')
+const recipeMenu = document.querySelector('#recipe-menu')
+const searchIngredientForm = document.querySelector('#search-form')
 
-function getRecipeData() {
-    fetch(baseUrl)
+// EventListeners
+searchIngredientForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let ingredient = document.querySelector('#ingredient-search').value;
+    console.log(ingredient)
+    getRecipeData(ingredient);
+    searchIngredientForm.reset();
+})
+
+// Fetch Functions
+function getRecipeData(ingredient) {
+    let newUrl = baseUrl + `${ingredient}`
+    fetch(newUrl)
     .then(r => r.json())
-    .then(console.log)
+    .then(mealsObjectWithArray => {
+        let mealsArray = mealsObjectWithArray.meals;
+        recipeMenu.innerHTML = '',
+        console.log(mealsArray)
+        mealsArray.forEach(meal => {
+            
+            let newMealObject = {
+                id: meal.idMeal,
+                image: meal.strMealThumb,
+                title: meal.strMeal,
+            }
+
+            let mealImage = newMealObject.image;
+            let mealTitle = newMealObject.title;
+            let mealImageDom = document.createElement('img');
+            mealImageDom.src = mealImage;
+            mealImageDom.alt = mealTitle;
+            recipeMenu.appendChild(mealImageDom);
+            
+        })
+    })
 }
 
-getRecipeData();
+// Initializers
+function init() {
+    document.addEventListener('DOMContentLoaded', () => {
+        getRecipeData();
+    })
+}
+init();
 
-// like bttn
-function likeBttn(x) {
-    x.classList.toggle("fa-thumbs-down");
-  }
